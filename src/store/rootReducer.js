fa57f60194a1cfbf2fc3a980a100ef202dbebbe9
Lucas Zluhan <lszluhan@hotmail.com>
2022-04-05 19:57:01 -0300

    mudanca para growbase

--- src/store/rootReducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import counterReducer from './modules/counter/slice';
-
-export const reducers = combineReducers({
-    counter: counterReducer,
-});
